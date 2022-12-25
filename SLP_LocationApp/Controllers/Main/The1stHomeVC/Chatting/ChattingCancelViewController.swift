4441ef0a1ddc1a63da0762ca90c60abfffd1fc40
michelle <lune111200@gmail.com>
2022-12-25 20:42:54 +0900

    Modify ChattingVC

--- SLP_LocationApp/Controllers/Main/The1stHomeVC/Chatting/ChattingCancelViewController.swift
+++ SLP_LocationApp/Controllers/Main/The1stHomeVC/Chatting/ChattingCancelViewController.swift
@@ -44,30 +44,7 @@ final class ChattingCancelViewController: BaseViewController {
         viewModel.studydodgeVM(otheruid: otheruid) { statusCode in
             switch statusCode {
             case APIStudyDodgeStatusCode.success.rawValue:
-                self.viewModel.checkMatchStateVM { myQueueState, statusCode in
-                    switch statusCode {
-                    case APIMyQueueStatusCode.success.rawValue:
-                        //홈화면으로 이동
-                        return
-                    case APIMyQueueStatusCode.noSearch.rawValue:
-                        return
-                    case APIMyQueueStatusCode.firebaseTokenError.rawValue:
-                        UserViewModel().refreshIDToken { isSuccess in
-                            if isSuccess! {
-                                self.studyDodge()
-                            } else {
-                                self.showToast(message: "네트워크 연결을 확인해주세요. (Token 갱신 오류)")
-                            }
-                        }
-                        return
-                    case APIMyQueueStatusCode.serverError.rawValue, APIMyQueueStatusCode.clientError.rawValue:
-                        print("서버 점검중입니다. 관리자에게 문의해주세요.")
-                        self.showToast(message: "서버 점검중입니다. 관리자에게 문의해주세요.")
-                        return
-                    default: self.showToast(message: "네트워크 연결을 확인해주세요.")
-                        return
-                    }
-                }
+                self.checkMatchState()
                 return
             case APIStudyDodgeStatusCode.wrongOtherUid.rawValue:
                 print("wrongOtherUid")
@@ -82,6 +59,48 @@ final class ChattingCancelViewController: BaseViewController {
                 }
                 return
             case APIStudyDodgeStatusCode.serverError.rawValue, APIStudyDodgeStatusCode.clientError.rawValue:
+                self.showToast(message: "서버 점검중입니다. 관리자에게 문의해주세요.")
+                return
+            default: self.showToast(message: "네트워크 연결을 확인해주세요.")
+                return
+            }
+        }
+    }
+    
+    func checkMatchState() {
+        self.viewModel.checkMatchStateVM { myQueueState, statusCode in
+            switch statusCode {
+            case APIMyQueueStatusCode.success.rawValue:
+                return
+            case APIMyQueueStatusCode.noSearch.rawValue: //일반상태
+                //홈화면으로 이동
+                var vcList = self.navigationController!.viewControllers
+                var count = 0
+                for vc in vcList {
+                    if vc.isKind(of: FindTotalViewController.self) ||
+                        vc.isKind(of: NearViewController.self) ||
+                        vc.isKind(of: NearPopUpViewController.self) ||
+                        vc.isKind(of: ReceivedRequestViewController.self) ||
+                        vc.isKind(of: ReceivedPopUpViewController.self) ||
+                        vc.isKind(of: ChattingViewController.self) {
+                        vcList.remove(at: count)
+                        continue
+                    }
+                    count = count + 1
+                }
+                self.navigationController!.viewControllers = vcList
+                return
+            case APIMyQueueStatusCode.firebaseTokenError.rawValue:
+                UserViewModel().refreshIDToken { isSuccess in
+                    if isSuccess! {
+                        self.studyDodge()
+                    } else {
+                        self.showToast(message: "네트워크 연결을 확인해주세요. (Token 갱신 오류)")
+                    }
+                }
+                return
+            case APIMyQueueStatusCode.serverError.rawValue, APIMyQueueStatusCode.clientError.rawValue:
+                print("서버 점검중입니다. 관리자에게 문의해주세요.")
                 self.showToast(message: "서버 점검중입니다. 관리자에게 문의해주세요.")
                 return
             default: self.showToast(message: "네트워크 연결을 확인해주세요.")
